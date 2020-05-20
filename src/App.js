687f1f953edb3717eb1d670b2841ff16388090d5
Hao <tangsihao1998@gmail.com>
2020-05-20 15:51:18 +0700

    add homepage for project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-// import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage';
 
 import { BrowserRouter as Router,Route,Switch }  from 'react-router-dom'
 import { withRouter } from 'react-router'
@@ -27,7 +27,7 @@ class App extends React.Component{
         <div className="App">
           <Scroll>
             <Switch>
-              {/* <Route exact path='/' component={HomePage} /> */}
+              <Route exact path='/' component={HomePage} />
             </Switch>
           </Scroll>
         </div>
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react';
+
+import "./HomePage.scss";
+
+// import components
+// import Header from './../../components/Header';
+// import Footer from './../../components/Footer';
+// // import Router
+// import { Link } from 'react-router-dom';
+
+class HomePage extends PureComponent {
+  render() {
+    return (
+      <div className="HomePage">
+        {/* HEADER */}
+        {/* <Header {...this.props} /> */}
+        
+        {/* FOOTER */}
+        {/* <Footer {...this.props}/> */}
+      </div>
+    )
+  }
+}
+
+export default HomePage;

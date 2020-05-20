cac6996fc83f278aa5d7a5093107f483ba41c9dd
Hao <tangsihao1998@gmail.com>
2020-05-20 16:29:02 +0700

    style header in project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,27 @@
 import React, { PureComponent } from 'react';
 import './Header.scss';
 
-// import redux
-import { connect } from 'react-redux';
-import actions from './../../redux/actions';
-import selectors from './../../redux/selectors';
-
 // import components
-import Login from './../Login';
+// import Login from './../Login';
 
 class Header extends PureComponent {
-  constructor(props){
-    super(props);
-  }
 
   render() {
     return (
       <React.Fragment>
         <header className="HomePage__Header">
           {/* Logo */}
-          <img className="HomePage__Logo" src={process.env.PUBLIC_URL + "/png/Logo.png"} alt="Logo"></img>
-          <div className="HomePage__Header--navigation">
-            {/* Link */}
-            <div className="Header--link">TRANG CHỦ</div>
-            <div className="Header--link">VỀ CHÚNG TÔI</div>
-            <div className="Header--link">LIÊN HỆ</div>
-          </div> 
+          <img className="Header__Logo" src={process.env.PUBLIC_URL + "/png/Logo.png"} alt="Logo"></img>
+          {/* Link */}
+          <div className="Header__link">TRANG CHỦ</div>
+          <div className="Header__link">VỀ CHÚNG TÔI</div>
+          <div className="Header__link">LIÊN HỆ</div>
           {/* check IF Đăng Nhập or not */}
-          <Login />
+          {/* <Login /> */}
         </header>
       </React.Fragment>
     )
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  dispatch,
-  // setCurrentTeam: currentTeam => dispatch(actions.setCurrentTeam(currentTeam)),
-  // setCurrentUser: currentUser => dispatch(actions.setCurrentUser(currentUser)),
-  // setUserTeams: teams => dispatch(actions.setUserTeams(teams)),
-});
-
-const mapStateToProps = state => ({
-  // currentUser: selectors.getCurrentUser(state),
-});
-
-export default connect(mapDispatchToProps, mapStateToProps)(Header);
+export default Header;

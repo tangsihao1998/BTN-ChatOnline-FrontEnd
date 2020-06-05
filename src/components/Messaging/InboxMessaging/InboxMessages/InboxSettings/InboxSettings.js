21507ca10f95429b4bd91256e1ec3d6c753c3336
Phạm Viết Lực <PhamVietLuc@admins-MacBook-2.local>
2020-06-05 12:52:02 +0700

    create InboxChap and Inbox Setting

---
+++ src/components/Messaging/InboxMessaging/InboxMessages/InboxSettings/InboxSettings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class InboxSettings extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                inbox settings
+            </React.Fragment>
+        )
+    }
+}
+
+export default InboxSettings

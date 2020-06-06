4289534a96ad93516effc6bcfd391acb6c6d93fb
Luc <lucqng111@gmail.com>
2020-06-06 16:45:36 +0700

    update chat UI

--- src/components/Messaging/InboxMessaging/InboxMessages/InboxMessages.js
+++ src/components/Messaging/InboxMessaging/InboxMessages/InboxMessages.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
+// import Row from 'react-bootstrap/Row'
+// import Container from 'react-bootstrap/Container'
+// import Col from 'react-bootstrap/Col'
 import InboxChat from './InboxChat/InboxChat'
 import InboxSettings from './InboxSettings/InboxSettings'
 import './InboxMessges.scss'
@@ -9,17 +9,17 @@ import './InboxMessges.scss'
 class InboxMessages extends Component {
     render() {
         return (
-            <Container style={{maxWidth: '100%',height:'100%'}}>
-                <Row id="rowHeader">Name : </Row>
-                <Row style={{height:'100%'}}>
-                    <Col xs={8}>
-                        <InboxChat></InboxChat>
-                    </Col>
-                    <Col xs={4}>
+            <div className="Inbox_Container" style={{maxWidth: '100%'}}>
+                <div id="rowHeader">Name : </div>
+                <div className="Container_Inbox_Chat_Settings" style={{height:'100%'}}>
+                    <div className="Inbox_Chat">
+                        <InboxChat></InboxChat>                     
+                    </div>
+                    <div className="Inbox_Settings">
                         <InboxSettings></InboxSettings>
-                    </Col>
-                </Row>
-            </Container>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/components/Messaging/InboxMessaging/InboxPeople/InboxPeople.js
+++ src/components/Messaging/InboxMessaging/InboxPeople/InboxPeople.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Row from 'react-bootstrap/Row'
+// import Row from 'react-bootstrap/Row'
 import './InboxPeople.scss'
 
 
@@ -11,51 +11,27 @@ class InboxPeople extends Component {
 
     render() {
         return (
-            <React.Fragment>
-                <Row style={{height: '100px', padding:'0',borderBottom:'solid',borderWidth:'1px',borderColor:'#c4c4c4'}}>
+            <div className="Inbox_People">
+                <div className="InboxPeople_Title" style={{height: '100px', padding:'0',borderBottom:'solid',borderWidth:'1px',borderColor:'#c4c4c4'}}>
                     Chat
-                </Row>
-                <Row>
+                </div>
+                <div className="InboxPeople_List">
                     <div class="chat_list">
                         <div class="chat_people" onClick={this.clickPeople}>
                             <div class="chat_img"> 
                                 <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></img>
                             </div>
+                            <div>
                             <div class="chat_ib">
-                            <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                            <p>Test, which is a new approach to have all solutions 
-                                astrology under one roof.</p>
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions 
+                                    astrology under one roof.</p>
+                            </div>
                             </div>
                         </div>
                     </div>
-                    <div class="chat_list">
-                        <div class="chat_people">
-                            <div class="chat_img"> 
-                                Day la anh 2
-                                {/* <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil">  */}
-                            </div>
-                            <div class="chat_ib">
-                            <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                            <p>Test, which is a new approach to have all solutions 
-                                astrology under one roof.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="chat_list">
-                        <div class="chat_people">
-                            <div class="chat_img"> 
-                                Day la anh 3
-                                {/* <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil">  */}
-                            </div>
-                            <div class="chat_ib">
-                            <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                            <p>Test, which is a new approach to have all solutions 
-                                astrology under one roof.</p>
-                            </div>
-                        </div>
-                    </div>
-                </Row>
-            </React.Fragment>
+                </div>
+            </div>
         )
     }
 }

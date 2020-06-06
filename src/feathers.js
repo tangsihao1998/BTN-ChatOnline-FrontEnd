2c10bcfc469718d39a22adf1e05a2b82ec96d670
Genesis <genesisrhapsodos98@gmail.com>
2020-06-06 21:37:58 +0700

    Fix: doesn't subscribe to all events by default

--- src/feathers.js
+++ src/feathers.js
@@ -6,8 +6,29 @@ const socket = io(SOCKET_IO_URLS);
 const client = feathers();
 
 client.configure(feathers.socketio(socket));
-client.configure(feathers.authentication({
-  storage: window.localStorage
-}));
+client.configure(
+	feathers.authentication({
+		storage: window.localStorage,
+	})
+);
+
+client.mixins.push((service, path) => {
+	service.mixin({
+		on(...args) {
+			const event = args[0];
+
+			// If it is a socket client service
+			if (service.connection && typeof service.connection.emit === 'function') {
+				service.connection.emit('subscribe', {
+					path,
+					event,
+				});
+			}
+
+			// Call the old .on
+			return this._super(...args);
+		},
+	});
+});
 
 export default client;

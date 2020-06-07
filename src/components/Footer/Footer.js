7d6a1edf84cd602a4077c3816a15c01a877df674
Luc <lucqng111@gmail.com>
2020-06-07 15:46:26 +0700

    Add Footer for website

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,25 @@ import './Footer.scss';
 class Footer extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <footer className="Footer">
+        <div className="Footer_Top">
+            <div className="About_TalkDer">
+                  <h3 className="TalkDer">TalkDer</h3>
+                  <h3 className="TalkDer_Description">Free app for connecting with your friends</h3>
+            </div>
+            <div className="Contact_Us">
+                  <h3 className="Label_Contactus">Contact Us</h3>
+                  <ul className="List_Student" style={{listStyle: 'none'}}>
+                    <li>16530 : Tăng Sĩ Hào</li>
+                    <li>16530 : Lý Gia Lập</li>
+                    <li>16530 : Phạm Viết Lực</li>
+                  </ul>
+            </div>
+        </div>
+        <div className="Footer_Bottom_Copyright">
+            <div className="Copyright_Text"> Copyright ©2020</div>
+        </div>
+      </footer>
     )
   }
 }

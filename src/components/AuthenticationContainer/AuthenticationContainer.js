283c0f8b1f51da0ab413c6b3d458606156ed1d11
Hao <tangsihao1998@gmail.com>
2020-06-07 15:29:02 +0700

    add responsive for introduction component and fix route path for authentication route

--- src/components/AuthenticationContainer/AuthenticationContainer.js
+++ src/components/AuthenticationContainer/AuthenticationContainer.js
@@ -8,16 +8,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import components
 import LoginForm from './../LoginForm';
 import RegisterForm from './../RegisterForm';
-
+import ForgotPasswordForm from './../ForgotPasswordForm';
 class AuthenticationContainer extends Component {
+
+  componentDidMount () {
+    const { history } = this.props;
+    if(history.location.pathname === '/authentication') {
+      history.push('/authentication/signin');
+    }
+  }
+
   render() {
     return (
       <div className="AuthenticationContainer">
         <Switch>
-          <Route exact path='/authentication/signin' render={(props) => <LoginForm {...props}/>} />
+          <Route exact path='/authentication/signin' component={LoginForm} />
           <Route exact path='/authentication/register' component={RegisterForm} />
-          {/* <Route exact path='/authentication/signin' render={(props) => <SignInForm {...props} pageTitle={pageTitle} />} ></Route>
-          <Route exact path='/authentication/profile' render={(props) => <UserInfoForm {...props} pageTitle={pageTitle} />} ></Route> */}
+          <Route exact path='/authentication/forgotpassword' component={ForgotPasswordForm} />
         </Switch>
       </div>
     )

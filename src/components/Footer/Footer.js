2489ab13b83fd3ce6e9dfb410ded9018b9bd29a1
Hao <tangsihao1998@gmail.com>
2020-06-08 11:59:33 +0700

    fix footer scss

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,21 +7,21 @@ class Footer extends Component {
     return (
       <footer className="Footer">
         <div className="Footer_Top">
-            <div className="About_TalkDer">
-                  <h3 className="TalkDer">TalkDer</h3>
-                  <h3 className="TalkDer_Description">Free app for connecting with your friends</h3>
-            </div>
-            <div className="Contact_Us">
-                  <h3 className="Label_Contactus">Contact Us</h3>
-                  <ul className="List_Student" style={{listStyle: 'none'}}>
-                    <li>16530 : Tăng Sĩ Hào</li>
-                    <li>16530 : Lý Gia Lập</li>
-                    <li>16530 : Phạm Viết Lực</li>
-                  </ul>
-            </div>
+          <div className="About_TalkDer">
+            <h3 className="TalkDer">Talk-der</h3>
+            <h3 className="TalkDer_Description">Feel free to connect with your friends</h3>
+          </div>
+          <div className="Contact_Us">
+            <h3 className="Label_Contactus">Contact Us</h3>
+            <ul className="List_Student">
+              <li>16530 : Tăng Sĩ Hào</li>
+              <li>16530 : Lý Gia Lập</li>
+              <li>16530 : Phạm Viết Lực</li>
+            </ul>
+          </div>
         </div>
         <div className="Footer_Bottom_Copyright">
-            <div className="Copyright_Text"> Copyright ©2020</div>
+          <div className="Copyright_Text"> Copyright ©2020</div>
         </div>
       </footer>
     )

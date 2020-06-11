cff0b9c0b5b0bac3f3cd15129ab05d7bc26904bc
Si Hao <tangsihao1998@gmail.com>
2020-06-11 13:20:04 +0700

    add message

--- src/components/InboxChat/InboxChat.js
+++ src/components/InboxChat/InboxChat.js
@@ -5,15 +5,40 @@ import './InboxChat.scss'
 import { IconButton } from '@material-ui/core';
 import { Send } from '@material-ui/icons';
 
+import { connect } from 'react-redux';
+import { services } from './../../feathers';
+
+import selectors from './../../redux/selectors';
+import actions from './../../redux/actions';
+
 // import components
 import MessageHistory from './../MessageHistory';
 
 class InboxChat extends Component {
-    handleSubmitMessage = (e) => {
-        e.preventDefault();
-        // Submit Message Here
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: '',
+        }
     }
+
+    handleTextChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+    };
+    
+    handleSubmitMessage = async (e) => {
+        e.preventDefault();
+        const { message } = this.state;
+
+        const { roomId } = this.props;
+        await this.props.createMessage(message, 'text', roomId)
+        // Submit Message Here
+        this.setState({ message: ''});
+    }
+
     render() {
+        const { message } = this.state;
         return (
             <div className="Inbox__Chat">
                 {/* Khung Message History */}
@@ -21,7 +46,16 @@ class InboxChat extends Component {
                 {/* Khung Input */}
                 <div className="Inbox_Input">
                     <form className='Input__form' onSubmit={this.handleSubmitMessage} >
-                        <textarea type="textarea" className="Form__input" placeholder="Type a message" cols='1' rows='1'/>
+                        <textarea 
+                            type="textarea" 
+                            className="Form__input" 
+                            placeholder="Type a message" 
+                            cols='1' 
+                            rows='1'
+                            name='message'
+                            value={message}
+                            onChange={this.handleTextChange}
+                        />
                         <IconButton edge="start" className="Icon__dropdown" color="inherit" aria-label="menu" type="submit">
                             <Send />
                         </IconButton>
@@ -32,4 +66,17 @@ class InboxChat extends Component {
     }
 }
 
-export default InboxChat;
+const mapStateToProps = (state) => ({
+    state,
+    currentUser: selectors.getCurrentUser(state),
+    roomId: selectors.getRoomId(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    dispatch,
+    createMessage: async(message, type, roomId) => {
+        await dispatch(services.messages.create({content: message, type: type, inRoom: roomId}))
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(InboxChat);

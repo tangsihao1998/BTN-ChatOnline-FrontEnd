9c3ebfd114963993a8702ea1661f0ef36def63b2
Si Hao <tangsihao1998@gmail.com>
2020-06-11 09:49:54 +0700

    verify email success

--- src/components/VerifyAccount/VerifyAccount.js
+++ src/components/VerifyAccount/VerifyAccount.js
@@ -9,26 +9,40 @@ class VerifyAccount extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // dùng để build giao diện 
       isVerified: false,
     }
-   
   }
 
   async componentDidMount () {
-    const params = new URLSearchParams(this.props.location.search);
-    const token = params.get('token');
-    // Get action
-    const getAction = this.props.history.location.pathname;
-    const action = getAction.slice(1,getAction.length);
-
-    const result = await this.props.onCreate(action, token);
-    console.log("VerifyAccount -> componentDidMount -> result", result)
+    try {
+      const params = new URLSearchParams(this.props.location.search);
+      const token = params.get('token');
+      // Get action
+      const getAction = this.props.history.location.pathname;
+      const action = getAction.slice(1,getAction.length);
+  
+      await this.props.onCreate(action, token);
+      alert('Verify Success');
+    } catch (error) {
+      switch(err.code) {
+        case 400: {
+          alert ('Actions does not exist')
+          break;
+        };
+        default: {
+          alert ('An unknown error has occured')
+          break;
+        };
+      }
+    }
+    this.props.history.push('/');
   }
   render() {
     return (
-      <div>
+      <React.Fragment>
         
-      </div>
+      </React.Fragment>
     )
   }
 }
